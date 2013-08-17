75e0be6926466e7404f3c90abb0c8018447bd600
Particle4Dev <steve@p4d-Vostro-270.(none)>
2013-08-17 18:33:04 +0700

    add fiber

--- em.js
+++ em.js
@@ -1,9 +1,17 @@
 if (Meteor.isClient) {
   Template.hello.events({
-    'click button': function(event){
+    'click button.future': function(event){
       event.preventDefault();
-      console.log('hello');
-      Meteor.call('asyncJob', 'fromClient', function(err, res){
+      console.log('before future');
+      Meteor.call('asyncFuture', 'fromClient', function(err, res){
+        console.log(err);
+        console.log(res);
+      });
+    },
+    'click button.fiber': function(event){
+      event.preventDefault();
+      console.log('before fiber');
+      Meteor.call('asyncFiber', 'fromClient', function(err, res){
         console.log(err);
         console.log(res);
       });
@@ -12,16 +20,16 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  //var Fiber = require('fibers');
+  var Fiber = Npm.require('fibers');
   var Future = Npm.require('fibers/future');
   Meteor.methods({
-    asyncJob: function(message) {
+    asyncFuture: function(message) {
 
       // Set up a future
       var fut = new Future();
 
       // This should work for any async method
-      setTimeout(function() {
+      Meteor.setTimeout(function() {
 
         // Return the results
         fut.return(message + " (delayed for 3 seconds)");
@@ -31,6 +39,17 @@ if (Meteor.isServer) {
       // Wait for async to finish before returning
       // the result
       return fut.wait();
+    },
+    asyncFiber: function (message) {
+      var fiber = Fiber.current;
+
+      Meteor.setTimeout(function () {
+        
+        fiber.run(message + " (delayed for 3 seconds)");
+      
+      }, 3 * 1000);      
+ 
+      return Fiber.yield();
     }
   });
 }
